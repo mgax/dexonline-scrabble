a47c72d2998cf4917109a1f08c04fa95beadef76
Alex Morega <alex@grep.ro>
2013-07-07 14:42:06 +0200

    map local chars

--- static/scrabble.js
+++ static/scrabble.js
@@ -10,10 +10,34 @@ $.get(SD.STATIC + 'forme.json').done(function(data) {
     });
 });
 
+
+var letter_map = {
+    'Ș': 'S',
+    'Ş': 'S',
+    'Ț': 'T',
+    'Ţ': 'T',
+    'Ă': 'A',
+    'Î': 'I',
+    'Â': 'A'
+};
+
+
+var normalize = function(word) {
+    var new_word = '';
+    _(word.toUpperCase()).forEach(function(letter) {
+        if(letter_map[letter]) {
+            letter = letter_map[letter];
+        }
+        new_word += letter;
+    });
+    return new_word;
+};
+
+
 var form = $('form[name=scrabble]');
 form.submit(function(evt) {
     evt.preventDefault();
-    var word = form.find('[name=q]').val().toUpperCase();
+    var word = normalize(form.find('[name=q]').val());
     var out = form.find('p');
     if(! words) {
         out.text('se încarcă');
